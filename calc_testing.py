470b69cc4fea3c1e5198fe1e61277c1709c3a1ee
SharpdotEXE <69698890+SharpdotEXE@users.noreply.github.com>
2021-01-13 10:39:57 -0600

    Update calc_testing.py

--- calc_testing.py
+++ calc_testing.py
@@ -1,21 +1,48 @@
 stack = []
 
 num1 = []
+op = []
 num2 = []
+
+strnum1 = []
 
 arithmetic = input('enter value, operator, then value: ')
 
 for i in arithmetic:
 	stack.append(i)
 
-stack = ''.join(stack)
+strstack = ''.join(stack)
 
-print(stack)
 
-for i in stack:
-	print(i)
-	if i == int(i):
-		num1.append(i)
-	
 
-print(num1)
+for i in strstack:
+
+	try:
+		num1.append(int(i))
+
+	except:
+		operation = stack.index(i)
+		newop = i
+		op.append(i)
+		break
+
+for i in num1:
+	strnum1.append(str(i))
+
+
+num2 = stack[operation:]
+num2.pop(0)
+
+
+num1 = ''.join(strnum1)
+operation = newop
+num2 = ''.join(num2)
+
+
+
+
+print(f'num1 = {num1}')
+print(f'operation = {operation}')
+print(f'num2 = {num2}')
+
+print(f'\nstack = {stack}')

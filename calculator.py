3618c9fd6e0c0d718abe81f8c4c1d4d818e33361
SharpdotEXE <69698890+SharpdotEXE@users.noreply.github.com>
2021-01-17 18:08:38 -0600

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,7 +3,7 @@ import tkinter as tk
 
 root = tk.Tk()
 root.title('Greg\'s Calculator')
-root.config(bg='darkgray')
+root.config(bg='#414241')
 
 stack = []
 
@@ -27,13 +27,12 @@ def equals():
     # might add try: except here
     str_stack = ''.join(stack)
     display_text.set(eval(''.join(stack)))
+    return stack
 
 
-def square():
-    # needs work
-    stack.append('**(2)')
-    display_text.set(''.join(stack))
-
+def set():
+    #needs work
+    stack = equals()
 
 
 def light_mode():
@@ -60,11 +59,12 @@ def light_mode():
     square_button.config(bg='white', fg='black')
     open_parenthesis_button.config(bg='white', fg='black')
     close_parenthesis_button.config(bg='white', fg='black')
-    light_mode_button.config(bg='black', fg='white')
+    set_button.config(bg='white', fg='#2a362e')
+    light_mode_button.config(bg='black', fg='#33f273')
     dark_mode_button.config(bg='white', fg='black')
 
     display_frame.config(bg='gray')
-    clear_del_frame.config(bg='gray')
+    clear_del_set_frame.config(bg='gray')
     one_4_7_frame.config(bg='gray')
     two_5_8_frame.config(bg='gray')
     three_6_9_frame.config(bg='gray')
@@ -99,17 +99,16 @@ def dark_mode():
     close_parenthesis_button.config(bg='#2a362e', fg='#33f273')
     light_mode_button.config(bg='#2a362e', fg='#33f273')
     dark_mode_button.config(bg='#33f273', fg='black')
+    set_button.config(bg='#2a362e', fg='#33f273')
 
     display_frame.config(bg='black')
-    clear_del_frame.config(bg='black')
+    clear_del_set_frame.config(bg='black')
     one_4_7_frame.config(bg='black')
     two_5_8_frame.config(bg='black')
     three_6_9_frame.config(bg='black')
     operator_frame.config(bg='black')
     on_off_frame.config(bg='black')
     equal_parentheses_frame.config(bg='black')
-
-
 
 ###########button functions###########
 
@@ -119,28 +118,28 @@ def dark_mode():
 
 
 ################frames################
-display_frame = tk.Frame(root, bg='darkgray', borderwidth=5)
+display_frame = tk.Frame(root, bg='#414241', borderwidth=5)
 display_frame.pack()
 
-clear_del_frame = tk.Frame(root, bg='darkgray', borderwidth=5)
-clear_del_frame.pack()
+clear_del_set_frame = tk.Frame(root, bg='#414241', borderwidth=5)
+clear_del_set_frame.pack()
 
-one_4_7_frame = tk.Frame(root, bg='darkgray', borderwidth=5)
+one_4_7_frame = tk.Frame(root, bg='#414241', borderwidth=5)
 one_4_7_frame.pack(side='left')
 
-two_5_8_frame = tk.Frame(root, bg='darkgray', borderwidth=5)
+two_5_8_frame = tk.Frame(root, bg='#414241', borderwidth=5)
 two_5_8_frame.pack(side='left')
 
-three_6_9_frame = tk.Frame(root, bg='darkgray', borderwidth=5)
+three_6_9_frame = tk.Frame(root, bg='#414241', borderwidth=5)
 three_6_9_frame.pack(side='left')
 
-operator_frame = tk.Frame(root, bg='darkgray', borderwidth=5)
+operator_frame = tk.Frame(root, bg='#414241', borderwidth=5)
 operator_frame.pack(side='right')
 
-equal_parentheses_frame = tk.Frame(root, bg='darkgray', borderwidth=5)
+equal_parentheses_frame = tk.Frame(root, bg='#414241', borderwidth=5)
 equal_parentheses_frame.pack(side='bottom', anchor='sw')
 
-on_off_frame = tk.Frame(root, bg='darkgray', borderwidth=5)
+on_off_frame = tk.Frame(root, bg='#414241', borderwidth=5)
 on_off_frame.pack()
 ################frames################
 
@@ -150,7 +149,7 @@ on_off_frame.pack()
 display_text = StringVar()
 
 display = tk.Label(display_frame, bg='black', textvariable=display_text,
-                   font=('fixedsys', 30), width=20)
+                   font=('fixedsys', 30), relief='raised', wraplength=360, width=20)
 
 display.pack()
 ###########labels#############
@@ -158,13 +157,20 @@ display.pack()
 
 
 ################buttons#################
-clearbutton = tk.Button(clear_del_frame, text='clear', padx=30, pady=10,
+clearbutton = tk.Button(clear_del_set_frame, text='Clear', padx=30, pady=10,
                         font=('fixedsys', 10), command=clear_stack)
+
 clearbutton.pack(side='left')
 
-delbutton = tk.Button(clear_del_frame, text='del', padx=30, pady=10,
+delbutton = tk.Button(clear_del_set_frame, text='Del', padx=30, pady=10,
                       font=('fixedsys', 10), command=delete_from_stack)
+
 delbutton.pack(side='right')
+
+set_button = tk.Button(clear_del_set_frame, text='Set', padx=22, pady=10,
+                       font=('fixedsys', 10), state='disabled', command=set)
+
+set_button.pack()
 
 number1_button = tk.Button(one_4_7_frame, text='1', padx=30, pady=10,
                            font=('fixedsys', 10), command=lambda: append_to_stack('1'))
@@ -202,6 +208,11 @@ number9_button = tk.Button(three_6_9_frame, text='9', padx=30, pady=10,
                            font=('fixedsys', 10), command=lambda: append_to_stack('9'))
 number9_button.pack()
 
+decimal_button = tk.Button(three_6_9_frame, text='.', padx=30, pady=10,
+                           font=('fixedsys', 10), command=lambda: append_to_stack('.'))
+
+decimal_button.pack()
+
 plus_button = tk.Button(operator_frame, text='+', padx=30, pady=10,
                         font=('fixedsys', 10), command=lambda: append_to_stack('+'))
 plus_button.pack()
@@ -221,17 +232,14 @@ divide_button.pack()
 number0_button = tk.Button(two_5_8_frame, text='0', padx=30, pady=10,
                         font=('fixedsys', 10), command=lambda: append_to_stack('0'))
 
-number0_button.pack(side='left')
+number0_button.pack()
 
 equal_button = tk.Button(equal_parentheses_frame, text='=', padx=30, pady=10,
                          font=('fixedsys', 10), command=equals)
 
-equal_button.pack(side='right')
+equal_button.pack(side='bottom', anchor='s')
 
-decimal_button = tk.Button(three_6_9_frame, text='.', padx=30, pady=10,
-                           font=('fixedsys', 10), command=lambda: append_to_stack('.'))
 
-decimal_button.pack()
 
 square_button = tk.Button(one_4_7_frame, text='^2', padx=26, pady=10,
                           font=('fixedsys', 10), command=lambda: append_to_stack('**2'))
@@ -241,12 +249,13 @@ square_button.pack()
 open_parenthesis_button = tk.Button(equal_parentheses_frame, text='(', padx=30, pady=10,
                                     font=('fixedsys', 10), command=lambda: append_to_stack('('))
 
-open_parenthesis_button.pack()
+open_parenthesis_button.pack(side='left', anchor='nw')
 
 close_parenthesis_button = tk.Button(equal_parentheses_frame, text=')', padx=30, pady=10,
                                      font=('fixedsys', 10), command=lambda: append_to_stack(')'))
 
-close_parenthesis_button.pack()
+close_parenthesis_button.pack(side='left', anchor='ne')
+
 
 light_mode_button = tk.Button(on_off_frame, text='Light Mode', padx=34, pady=10,
                       font=('fixedsys', 10), command=light_mode)
@@ -257,6 +266,8 @@ dark_mode_button = tk.Button(on_off_frame, text='Dark Mode', padx=38, pady=10,
                        font=('fixedsys', 10), command=dark_mode)
 
 dark_mode_button.pack()
+
+
 
 ################buttons#################
 
@@ -283,11 +294,13 @@ decimal_button.config(bg='white', fg='black')
 square_button.config(bg='white', fg='black')
 open_parenthesis_button.config(bg='white', fg='black')
 close_parenthesis_button.config(bg='white', fg='black')
-light_mode_button.config(bg='black', fg='white')
+set_button.config(bg='white', fg='#2a362e')
+light_mode_button.config(bg='black', fg='#33f273')
 dark_mode_button.config(bg='white', fg='black')
 
+
 display_frame.config(bg='gray')
-clear_del_frame.config(bg='gray')
+clear_del_set_frame.config(bg='gray')
 one_4_7_frame.config(bg='gray')
 two_5_8_frame.config(bg='gray')
 three_6_9_frame.config(bg='gray')
